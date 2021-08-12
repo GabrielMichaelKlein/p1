91de15b09bfc54eb3d53052f3ca9e42526c0e736
GabrielMichaelKlein <gabriel.michael.klein@gmail.com>
2021-08-12 13:35:57 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,62 @@
+# p1 - Problem Scenario
+
+## Project Description
+
+This Project involved solving a series of 6 problems which asked for information about a set of 6 different files. To accomplish this, the files had to be imported into a table which could be queried. To accomplish this, I connected IntelliJ to Spark to utilize Spark SQL as a quick querying tool.
+
+## Technologies Used
+
+* Scala - 2.11.12
+* Hadoop - 3.2
+* Spark - 3.1.2
+* IntelliJ
+* GitHub
+
+## Features
+
+List of features ready and TODOs for future development
+* Connects to Spark using Intellij
+* Imports data from a CSV into a table using Spark SQL
+* Combines data from multiple tables into one using Spark SQL
+
+To-do list:
+* Optimize performance for faster querying
+* Allow for user-defined problems to be solved
+
+## Getting Started
+
+Follow the steps in the link below to set up IntelliJ for use with Spark:
+
+- https://kaizen.itversity.com/setup-development-environment-intellij-and-scala-big-data-hadoop-and-spark/
+   
+Clone the repository onto your machine:
+
+- git clone https://github.com/GabrielMichaelKlein/p1
+
+In Intellij, run the program.
+
+## Usage
+
+Here are the problems that this project solves:
+
+Problem Scenario 1 
+- What is the total number of consumers for Branch1?
+- What is the number of consumers for the Branch2?
+
+Problem Scenario 2 
+- What is the most consumed beverage on Branch1
+- What is the least consumed beverage on Branch2
+- What is the Average consumed beverage of  Branch2
+
+Problem Scenario 3
+- What are the beverages available on Branch10, Branch8, and Branch1?
+- What are the comman beverages available in Branch4, Branch7?
+
+Problem Scenario 4
+- Create a partition,View for the scenario3.
+
+Problem Scenario 5
+- Alter the table properties to add "note","comment"
+
+Problem Scenario 6
+- Remove a row from any Senario.
--- src/main/scala/p1.scala
+++ src/main/scala/p1.scala
@@ -1,3 +1,11 @@
+/***************************************************************
+ * Gabriel Klein
+ * p1
+ * August 5, 2021
+ *
+ * This project practices Spark SQL
+ ***************************************************************/
+
 import org.apache.spark.sql.SparkSession
 object p1 {
   def main(args: Array[String]): Unit = {
@@ -25,64 +33,67 @@ object p1 {
 //    spark.sql("LOAD DATA LOCAL INPATH 'Input/Bev_ConscountC.txt' INTO TABLE bev_conscount")
 
     /* Scenario 1 (UNCOMMENT IF YOU WISH TO RUN)*/
-    println("Scenario 1\n")
-    println("Number of consumers from branch 1:")
-    spark.sql("SELECT SUM(num_consumers) AS b1_consumers FROM bev_branch b JOIN " +
-      "bev_conscount c ON b.Drink=c.Drink WHERE branch='Branch2'").show()
-    println("Number of consumers from branch 2:")
-    spark.sql("SELECT SUM(num_consumers) AS b2_consumers FROM bev_branch b JOIN " +
-      "bev_conscount c ON b.Drink=c.Drink WHERE branch='Branch2'").show()
-
-
-    /* Scenario 2 (UNCOMMENT IF YOU WISH TO RUN)*/
-    println("Scenario 2\n")
-    println("Most consumed drink from branch 1:")
-    spark.sql("WITH t1 AS (" +
-               "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN " +
-      "bev_conscount c ON b.Drink=c.Drink " +
-      "WHERE branch='Branch1' GROUP BY b.drink) " +
-      "SELECT drink FROM t1 WHERE num_orders=(SELECT MAX(num_orders) FROM t1)").show()
-
-    println("Least consumed drink from branch 2:")
-    spark.sql("WITH t1 AS (" +
-      "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN bev_conscount c ON b.Drink=c.Drink " +
-      "WHERE branch='Branch2' GROUP BY b.drink) " +
-      "SELECT drink FROM t1 WHERE num_orders=(SELECT MIN(num_orders) FROM t1)").show()
-
-    println("Drink that is consumed closest to the average amount from branch 2:")
-    spark.sql("WITH t1 AS (" +
-      "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN bev_conscount c ON b.Drink=c.Drink " +
-      "WHERE branch='Branch2' GROUP BY b.drink), t2 AS ( " +
-      "SELECT drink, ABS( num_orders - (SELECT AVG(num_orders) FROM t1)) AS mid FROM t1 ) " +
-      "SELECT drink FROM t2 WHERE mid=(SELECT MIN(mid) FROM t2)").show()
-
-    /* Scenario 3 */
-    println("Scenario 3:\n")
-    println("Beverages available on branches 10, 8, and 1:")
-    spark.sql("SELECT DISTINCT(drink) AS drinks FROM bev_branch WHERE " +
-      "branch='Branch1' OR branch='Branch8' OR branch='Branch9' ORDER BY drinks").show()
-
-    println("Beverages common from branches 4 and 7:")
-    spark.sql("WITH t1 AS (" +
-      "SELECT * FROM bev_branch WHERE branch='Branch4'), t2 AS (" +
-      "SELECT * FROM bev_branch WHERE branch='Branch7') " +
-      "SELECT drink FROM t1 INTERSECT SELECT drink FROM t2 ORDER BY drink").show()
+//    println("Scenario 1\n")
+//    println("Number of consumers from branch 1:")
+//    spark.sql("SELECT SUM(num_consumers) AS b1_consumers FROM bev_branch b JOIN " +
+//      "bev_conscount c ON b.Drink=c.Drink WHERE branch='Branch2'").show()
+//    println("Number of consumers from branch 2:")
+//    spark.sql("SELECT SUM(num_consumers) AS b2_consumers FROM bev_branch b JOIN " +
+//      "bev_conscount c ON b.Drink=c.Drink WHERE branch='Branch2'").show()
+//
+//
+//    /* Scenario 2 (UNCOMMENT IF YOU WISH TO RUN)*/
+//    println("Scenario 2\n")
+//    println("Most consumed drink from branch 1:")
+//    spark.sql("WITH t1 AS (" +
+//               "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN " +
+//      "bev_conscount c ON b.Drink=c.Drink " +
+//      "WHERE branch='Branch1' GROUP BY b.drink) " +
+//      "SELECT drink FROM t1 WHERE num_orders=(SELECT MAX(num_orders) FROM t1)").show()
+//
+//    println("Least consumed drink from branch 2:")
+//    spark.sql("WITH t1 AS (" +
+//      "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN bev_conscount c ON b.Drink=c.Drink " +
+//      "WHERE branch='Branch2' GROUP BY b.drink) " +
+//      "SELECT drink FROM t1 WHERE num_orders=(SELECT MIN(num_orders) FROM t1)").show()
+//
+//    println("Drink that is consumed closest to the average amount from branch 2:")
+//    spark.sql("WITH t1 AS (" +
+//      "SELECT b.Drink, SUM(num_consumers) AS num_orders FROM bev_branch b JOIN bev_conscount c ON b.Drink=c.Drink " +
+//      "WHERE branch='Branch2' GROUP BY b.drink), t2 AS ( " +
+//      "SELECT drink, ABS( num_orders - (SELECT AVG(num_orders) FROM t1)) AS mid FROM t1 ) " +
+//      "SELECT drink FROM t2 WHERE mid=(SELECT MIN(mid) FROM t2)").show()
+//
+//    /* Scenario 3 */
+//    println("Scenario 3:\n")
+//    println("Beverages available on branches 10, 8, and 1:")
+//    spark.sql("SELECT DISTINCT(drink) AS drinks FROM bev_branch WHERE " +
+//      "branch='Branch1' OR branch='Branch8' OR branch='Branch9' ORDER BY drinks").show()
+//
+//    println("Beverages common from branches 4 and 7:")
+//    spark.sql("WITH t1 AS (" +
+//      "SELECT * FROM bev_branch WHERE branch='Branch4'), t2 AS (" +
+//      "SELECT * FROM bev_branch WHERE branch='Branch7') " +
+//      "SELECT drink FROM t1 INTERSECT SELECT drink FROM t2 ORDER BY drink").show()
 
     /* Scenario 4 */
     println("Scenario 4\n")
 //    spark.sql("CREATE VIEW s3 AS SELECT DISTINCT(drink) AS drinks FROM bev_branch WHERE branch='Branch1' OR branch='Branch8' OR branch='Branch9' ORDER BY drinks")
+    spark.sql("SELECT COUNT(*) FROM s3").show()
     spark.sql("SELECT * FROM s3").show()
+
+//    spark.sql("DROP TABLE branch_part")
 //    spark.sql("CREATE TABLE branch_part(drink STRING) PARTITIONED BY (branch STRING)")
-//    spark.sql("INSERT INTO branch_part SELECT drink, branch FROM bev_branch")
+//    spark.sql("INSERT INTO branch_part SELECT drink, branch FROM bev_branch WHERE branch='Branch1' OR branch='Branch8' OR branch='Branch9'")
     spark.sql("SELECT * FROM branch_part ORDER BY branch").show()
     spark.sql("SHOW PARTITIONS branch_part").show()
 
-    /* Scenario 5 */
-    println("Scenario 5\n")
-//    spark.sql("CREATE TABLE branch_5(drink STRING, branch STRING)")
-//    spark.sql("INSERT INTO TABLE branch_5 SELECT drink, branch FROM bev_branch")
-//    spark.sql("ALTER TABLE branch_5 ADD COLUMNS (note STRING, comment STRING)")
-    spark.sql("SELECT * FROM branch_5 ORDER BY drink, branch").show()
+//    /* Scenario 5 */
+//    println("Scenario 5\n")
+////    spark.sql("CREATE TABLE branch_5(drink STRING, branch STRING)")
+////    spark.sql("INSERT INTO TABLE branch_5 SELECT drink, branch FROM bev_branch")
+////    spark.sql("ALTER TABLE branch_5 ADD COLUMNS (note STRING, comment STRING)")
+//    spark.sql("SELECT * FROM branch_5 ORDER BY drink, branch").show()
 
     /* Scenario 6 */
     println("Scenario 6\n")
